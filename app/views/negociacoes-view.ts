5d0542f682dec411f0cfbf0ff9ac551784f6c982
Daniel <danielribas1105@gmail.com>
2024-01-06 10:03:16 -0300

    table with rows about negotiations

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -1,6 +1,14 @@
+import { Negociacoes } from "../models/negociacoes.js";
+
 export class NegociacoesView {
 
-    template(): string {
+    private elemento: HTMLElement;
+
+    constructor (seletor: string) {
+        this.elemento = document.querySelector(seletor);
+    }
+
+    template(model: Negociacoes): string {
         return `
             <table class="table table-hover table-bordered">
                 <thead>
@@ -11,10 +19,26 @@ export class NegociacoesView {
                     </tr>
                 </thead>
                 <tbody>
-
+                    ${
+                        model.lista().map(negociacao => {
+                            return `
+                                <tr>
+                                    <td>${new Intl.DateTimeFormat().format(negociacao.data)}</td>
+                                    <td>${negociacao.quantidade}</td>
+                                    <td>${negociacao.valor}</td>
+                                </tr>
+                            `;
+                        }).join(' ')
+                    }
                 </tbody>
             </table>
         `;
+    }
+
+    update(model: Negociacoes): void {
+        const template = this.template(model);
+        console.log(template)
+        this.elemento.innerHTML = template;
     }
     
 }
